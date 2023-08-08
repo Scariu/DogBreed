9ac1285b698208922aec120d694c5a4db61e7009
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-08 19:13:35 -0400

    Se implementa DogBreedViewModel.

--- app/src/main/java/com/example/dogbreed/presentation/DogBreedViewModel.kt
+++ app/src/main/java/com/example/dogbreed/presentation/DogBreedViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.dogbreed.presentation
 
-class DogBreedViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dogbreed.data.Repository
+import com.example.dogbreed.data.local.DogBreedDAO
+import com.example.dogbreed.data.local.DogBreedDataBase
+import com.example.dogbreed.data.remote.DogBreedRetrofit
+import kotlinx.coroutines.launch
+
+class DogBreedViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: Repository
+
+    init {
+        val api = DogBreedRetrofit.getDogBreedRetrofit()
+        val dogBreedDataBase: DogBreedDAO = DogBreedDataBase.getDatabase(application).getBreedDAO()
+        repository = Repository(api, dogBreedDataBase)
+    }
+
+    fun getData() = viewModelScope.launch { repository.getBreeds() }
 }
