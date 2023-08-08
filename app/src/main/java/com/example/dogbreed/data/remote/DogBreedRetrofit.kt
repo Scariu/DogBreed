35d4f019b3eba222ffab8b48efc12203bb61bd38
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-07 21:22:34 -0400

    Se implementa class DogBreedRetrofit.

--- app/src/main/java/com/example/dogbreed/data/remote/DogBreedRetrofit.kt
+++ app/src/main/java/com/example/dogbreed/data/remote/DogBreedRetrofit.kt
@@ -1,4 +1,17 @@
 package com.example.dogbreed.data.remote
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class DogBreedRetrofit {
+    companion object {
+        private const val URL_BASE = "https://dog.ceo/api/"
+
+        fun getDogRaceRetrofit(): DogBreedAPI {
+            val mRetrofit = Retrofit.Builder().baseUrl(URL_BASE).addConverterFactory(
+                GsonConverterFactory.create()
+            ).build()
+            return mRetrofit.create(DogBreedAPI::class.java)
+        }
+    }
 }
