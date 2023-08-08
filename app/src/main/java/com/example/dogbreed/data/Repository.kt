c93dae7dcca151d4dd533d9e70d71d3356c69ec2
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-07 22:13:39 -0400

    Se implementa class Repository.

--- app/src/main/java/com/example/dogbreed/data/Repository.kt
+++ app/src/main/java/com/example/dogbreed/data/Repository.kt
@@ -1,4 +1,19 @@
 package com.example.dogbreed.data
 
-class Repository {
+import com.example.dogbreed.data.local.DogBreedDAO
+import com.example.dogbreed.data.local.DogBreedEntity
+import com.example.dogbreed.data.remote.DogBreedAPI
+
+class Repository(private val dogBreedAPI: DogBreedAPI, private val dogBreedDAO: DogBreedDAO) {
+    suspend fun getBreeds() {
+        val response = dogBreedAPI.getData() // Aqui llegan los datos
+        if (response.isSuccessful) { //Evalua si llegaron los datos
+            val message = response.body()!!.message // Solo obtiene el mensaje, no tiene status
+            val keys = message.keys
+            keys.forEach {
+                val dogBreedEntity = DogBreedEntity(it)
+                dogBreedDAO.insertDogBreed(dogBreedEntity)
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/dogbreed/data/local/DogBreedEntity.kt
+++ app/src/main/java/com/example/dogbreed/data/local/DogBreedEntity.kt
@@ -4,6 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "tabla_dogbreed")
-class DogBreedEntity (
-@PrimaryKey(autoGenerate = true) var id: Long,
-        val breed: String)
+class DogBreedEntity(val breed: String) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+}
