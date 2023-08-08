599f7cb67c4d06070f179e6c96bfcee5f718e32b
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-07 21:44:30 -0400

    Se modifica DogBreedDAO para su correcto funcionamiento.

--- app/src/main/java/com/example/dogbreed/data/local/DogBreedDAO.kt
+++ app/src/main/java/com/example/dogbreed/data/local/DogBreedDAO.kt
@@ -1,9 +1,11 @@
 package com.example.dogbreed.data.local
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 
+@Dao
 interface DogBreedDAO {
     @Insert
     suspend fun insertDogBreed(dogBreedEntity: DogBreedEntity)
