31f9351d708b4aa866159285cfc0dc025f7f6f1a
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-07 21:38:04 -0400

    Se implementa DogBreedEntity.

--- app/src/main/java/com/example/dogbreed/data/local/DogBreedEntity.kt
+++ app/src/main/java/com/example/dogbreed/data/local/DogBreedEntity.kt
@@ -1,4 +1,9 @@
 package com.example.dogbreed.data.local
 
-class DogBreedEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tabla_dogbreed")
+class DogBreedEntity (
+@PrimaryKey(autoGenerate = true) var id: Long,
+        val breed: String)
