2e744b79ab5207a701b8ee2772b72a6562348bce
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-07 21:41:55 -0400

    Se implementa class DogBreedDataBase.

--- app/src/main/java/com/example/dogbreed/data/local/DogBreedDataBase.kt
+++ app/src/main/java/com/example/dogbreed/data/local/DogBreedDataBase.kt
@@ -1,4 +1,33 @@
 package com.example.dogbreed.data.local
 
-class DogBreedDataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [DogBreedEntity::class], version = 1)
+abstract class DogBreedDataBase : RoomDatabase() {
+    abstract fun getBreedDAO(): DogBreedDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DogBreedDataBase? = null
+
+        fun getDatabase(context: Context): DogBreedDataBase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    DogBreedDataBase::class.java,
+                    "breeds_database"
+                ).build()
+
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
