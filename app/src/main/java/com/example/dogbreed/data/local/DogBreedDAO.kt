9f973265257d2e45b0f0cdc5a3363aa9d0876813
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-07 21:38:34 -0400

    Se implementa DogBreedDAO.

--- app/src/main/java/com/example/dogbreed/data/local/DogBreedDAO.kt
+++ app/src/main/java/com/example/dogbreed/data/local/DogBreedDAO.kt
@@ -1,4 +1,13 @@
 package com.example.dogbreed.data.local
 
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+
 interface DogBreedDAO {
+    @Insert
+    suspend fun insertDogBreed(dogBreedEntity: DogBreedEntity)
+
+    @Query("SELECT * FROM tabla_dogbreed order by breed ASC")
+    fun getDogBreeds(): LiveData<List<DogBreedEntity>>
 }
