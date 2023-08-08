14d25763a501c64e56473c9425edc78a2724e56e
Flopy <f.hidalgoalarcon@gmail.com>
2023-08-08 19:42:29 -0400

    Se implementa class AdapterItemBreed.

--- app/src/main/java/com/example/dogbreed/presentation/AdapterItemBreed.kt
+++ app/src/main/java/com/example/dogbreed/presentation/AdapterItemBreed.kt
@@ -1,4 +1,38 @@
 package com.example.dogbreed.presentation
 
-class AdapterItemBreed {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dogbreed.data.local.DogBreedEntity
+import com.example.dogbreed.databinding.ItemBreedBinding
+
+class AdapterItemBreed: RecyclerView.Adapter<AdapterItemBreed.ViewHolder>() {
+    private lateinit var itemBinding: ItemBreedBinding
+    private val lisItemBreed = mutableListOf<DogBreedEntity>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterItemBreed.ViewHolder {
+        itemBinding = ItemBreedBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: AdapterItemBreed.ViewHolder, position: Int) {
+        val breed = lisItemBreed[position]
+        holder.bind(breed)
+    }
+
+    override fun getItemCount(): Int {
+        return lisItemBreed.size
+    }
+
+    fun setData(breeds: List<DogBreedEntity>){
+        this.lisItemBreed.clear()
+        this.lisItemBreed.addAll(breeds)
+        notifyDataSetChanged()
+    }
+    class ViewHolder(private val binding: ItemBreedBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(breedView: DogBreedEntity) {
+            binding.tvBreed.text = breedView.breed
+           // binding.cvItemBreed.setOnClickListener{ }
+        }
+    }
 }
